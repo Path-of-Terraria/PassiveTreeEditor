044c3b310289e7a9a6d814411bf44606ef5d9b12
catNull <benvb2004@gmail.com>
2024-06-12 01:20:35 +0200

    feat: update to use internalIdentifier

--- LoveKeybindings/Shortcuts/Tree - t/Data - d/ReloadTrees.lua
+++ LoveKeybindings/Shortcuts/Tree - t/Data - d/ReloadTrees.lua
@@ -5,7 +5,13 @@ local MyKey = s:new()
 MyKey.key = "t"
 
 function MyKey:onActivate()
+    SelectedNode1 = nil
+    SelectedNode2 = nil
+    
+    local treeType = CurTree
+
     LoadTrees()
+    SetTree(treeType)
 end
 
 return MyKey
